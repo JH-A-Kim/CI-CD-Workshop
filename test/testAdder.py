298f3d777e5d2f0f62a99a47fe3cd361eb38e01e
jak28 <jak28@sfu.ca>
2025-12-16 16:37:23 -0800

    moved files

--- test/testAdder.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from adder import adder
-
-class TestAdder(unittest.TestCase):
-    def test_add_positive_numbers(self):
-        self.assertEqual(adder(2, 3), 5)
-
-if __name__ == '__main__':
-    unittest.main()
